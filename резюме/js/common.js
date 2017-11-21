a83a43a582f7618c8c0657fed21cc4d41006f726
sasha-klim <sasha.klim.2017@inbox.ru>
2017-11-21 09:50:29 +0300

    My Commit

---
+++ резюме/js/common.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+   
+
+    $('.menu-icon').click(function () {
+        $('nav ul').slideToggle(500);
+    });
+    $(window).resize(function () {
+        if ($(window).width() < 300) {
+            $('nav ul').removeAttr('style');
+        }
+    });
+
+    $('.contact').click(function () {
+        $('.contact_me').slideToggle(500);
+    });
+    $(window).resize(function () {
+        if ($(window).width() < 300) {
+            $('nav ul').removeAttr('style');
+        }
+    });
+    
+    $(".owl-carousel").owlCarousel({
+		loop: true,
+		responsive : {
+			0 : {
+			items : 1,
+			nav : true
+		}
+	 },
+	 navText: ""
+    });
+
+});
+$(window).scroll(function() {
+    $('.mov').each(function(){
+      var imagePos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+400) {
+        $(this).addClass('fadeInRight');
+      }
+    });
+    $('.move').each(function(){
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+          $(this).addClass('fadeInDown');
+        }
+      });
+  });
